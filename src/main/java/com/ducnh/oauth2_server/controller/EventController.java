ef945ecb453be4fd2af88e3b55f940eead8201c8
Duc111095 <101342718+Duc111095@users.noreply.github.com>
2025-04-02 14:48:45 +0700

    add features

--- src/main/java/com/ducnh/oauth2_server/controller/EventController.java
+++ src/main/java/com/ducnh/oauth2_server/controller/EventController.java
@@ -3,11 +3,13 @@ package com.ducnh.oauth2_server.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ducnh.oauth2_server.form.EventForm;
 import com.ducnh.oauth2_server.model.StravaEvent;
@@ -41,15 +43,15 @@ public class EventController {
     }
 	
 	@GetMapping("/events/{id}/delete")
-	public String deleteEvent(Model model, @PathVariable("id") String id) {
+	public ModelAndView  deleteEvent(ModelMap model, @PathVariable("id") String id) {
 		eventService.delete(id);
 		Iterable<StravaEvent> events = eventService.findAll();
 		model.addAttribute("events", events);
-		return "events";
-	}
+        return new ModelAndView("redirect:/events", model);
+     }
 	
 	@PostMapping("/events/{id}/edit")
-	public String updateEvent(Model model, @PathVariable("id") String id, @ModelAttribute("newEvent") EventForm newEvent) {
+	public ModelAndView updateEvent(ModelMap model, @PathVariable("id") String id, @ModelAttribute("newEvent") EventForm newEvent) {
 		StravaEvent oldEvents = eventService.findById(id).get();
 		oldEvents.setEventName(newEvent.getEventName());
 		oldEvents.setStartDate(newEvent.getStartDate());
@@ -62,6 +64,6 @@ public class EventController {
 
 		Iterable<StravaEvent> events = eventService.findAll();
 		model.addAttribute("events", events);
-		return "events";
+        return new ModelAndView("redirect:/events", model);
 	}
 }
