580e4912f167b2f6a4228b0573091f881122d703
Duc111095 <101342718+Duc111095@users.noreply.github.com>
2026-05-28 10:57:49 +0700

    add echo in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 
 #!/bin/bash
-mvn clean package && cp target/runner.war ~/apache-tomcat-9.0.102/webapps/
+mvn clean package && echo "Clean successfully!" && cp target/runner.war ~/apache-tomcat-9.0.102/webapps/ && echo "Copy successfully!"
